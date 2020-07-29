c5ace2c717b2f5b011478605a3a5e51fae813bf1
Charlotte Mannion <charlottemannion100@gmail.com>
2020-07-29 11:05:26 +0100

    extends the example to use classes

--- greeter.js
+++ greeter.js
@@ -1,5 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hello," + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "Charly", lastName: "Mannion" };
+var user = new Student("Charly", "S. R.", "Mannion");
 document.body.textContent = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,10 @@
+class Student {
+  fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
 interface Person {
   firstName: string;
   lastName: string;
@@ -7,6 +14,6 @@ function greeter(person: Person) {
   return "Hello," + person.firstName + " " + person.lastName;
 }
 
-let user = { firstName: "Charly", lastName: "Mannion"};
+let user = new Student ("Charly", "S. R.", "Mannion");
 
 document.body.textContent = greeter(user);
